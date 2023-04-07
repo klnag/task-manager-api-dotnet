66bf5797d078686c553986ad95d7bc57620e911e
Mohamed-Al-Asam <mohammed70945@icloud.com>
2023-04-07 18:30:09 +0300

    add jwt for the user

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using src.Helpers;
 using src.Models.UserModel;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace src.Controllers;
 
@@ -9,9 +13,21 @@ namespace src.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase {
     private readonly DataContext context;
+    private readonly IConfiguration _configuration;
 
-    public UserController(DataContext context) {
+    public UserController(IConfiguration configuration,DataContext context) {
+        this._configuration = configuration;
         this.context = context;
+    }
+
+    private string userCreateJwt(string id) {
+        List<Claim> claims = new List<Claim> {
+            new Claim(ClaimTypes.NameIdentifier, id)
+        };
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+        SigningCredentials cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        JwtSecurityToken token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: cred);
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 
     [HttpGet]
@@ -28,14 +44,27 @@ public class UserController : ControllerBase {
         return user; 
     }
     [HttpPost] 
-    public ActionResult<User> Post(User userFormBody) {
+    public ActionResult<string> Post(UserDto userFormBody) {
         if(context.Users?.FirstOrDefault(user => user.Email == userFormBody.Email) == null) {
-            User newUser = new User { Username = userFormBody.Username, Email = userFormBody.Email, Password = userFormBody.Password };
+            string hashPassword = BCrypt.Net.BCrypt.HashPassword(userFormBody.Password);
+            User newUser = new User { Username = userFormBody.Username, Email = userFormBody.Email, Password = hashPassword };
             context.Users?.Add(newUser);
             context.SaveChanges();
-            return newUser;
+            return userCreateJwt(newUser.Id+"");
         }
         return new BadRequestObjectResult("user with that email exist already exisit");
+    }
+
+    [HttpPost("login")]
+    public string post(UserDto userFormBody) {
+        User user = context.Users?.FirstOrDefault(user => user.Email == userFormBody.Email);
+        if(user == null) {
+            return "email does not exist";
+        }
+        if(!BCrypt.Net.BCrypt.Verify(userFormBody.Password, user.Password)){
+            return "Password is incorect";
+        }
+        return userCreateJwt(user.Id+"");
     }
 
     [HttpPatch("{id}")]
